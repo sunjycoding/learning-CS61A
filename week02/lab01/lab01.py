3050a5f598fd6b05ad7ea646c579126044f2d33e
sunjy <sunjyleisure@gmail.com>
2024-07-21 20:12:11 -0700

    feat: week02 - lab01

---
+++ week02/lab01/lab01.py
@@ -0,0 +1,105 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    total = n
+    if k == 0:
+        return 1
+    while k > 1:
+        total, n, k = total * (n - 1), n - 1, k - 1
+    return total
+
+def divisible_by_k(n, k):
+    """
+    >>> a = divisible_by_k(10, 2)  # 2, 4, 6, 8, and 10 are divisible by 2
+    2
+    4
+    6
+    8
+    10
+    >>> a
+    5
+    >>> b = divisible_by_k(3, 1)  # 1, 2, and 3 are divisible by 1
+    1
+    2
+    3
+    >>> b
+    3
+    >>> c = divisible_by_k(6, 7)  # There are no integers up to 6 divisible by 7
+    >>> c
+    0
+    """
+    "*** YOUR CODE HERE ***"
+    i, total = 1, 0
+    while i <= n:
+        if i % k == 0:
+            print(i)
+            total += 1
+        i = i + 1
+    return total
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    "*** YOUR CODE HERE ***"
+    sum = 0
+    while y >= 1:
+        digit = y % 10
+        sum += digit
+        y = y // 10
+    return sum
+        
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    digit = n % 10
+    n = n // 10
+    if n < 1:
+        return False
+    hasEight = False
+    if digit == 8:
+        hasEight = True
+    while n >= 1:
+        digit = n % 10
+        n = n // 10
+        if digit == 8 and hasEight:
+            return True
+        elif digit == 8:
+            hasEight = True
+        else:
+            hasEight = False
+    return False
